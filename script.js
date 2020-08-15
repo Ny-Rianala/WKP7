7cdf3d6a0b3d48ab99a1f697b01c93c27c968996
kati <fanomezantsoakatiuscia048@gmail.com>
2020-08-15 16:47:33 +0300

    added the add button

--- script.js
+++ script.js
@@ -22,64 +22,62 @@ const books = [
     },
 ];
 
-
 const descriptionForm = document.querySelector('.book-description');
 const list = document.querySelector('.list');
+const listOfBook = document.querySelector('.list-of-book');
+const addBooksForm = document.querySelector('.add-book-form');
+const addBtn = document.querySelector('.add-book');
+const allBooks = [];
 
-
-//Generate list for all foods
-const listOfBook = (e) => {
+//Generate list for the books in the object
+const listedBook = (e) => {
     //add html
-        const html = books.map (book => `
-          <table class= ".list">
-            <tr>
+    const html = books.map(book => `
+          <tr class= ".list">
               <td>${book.title}</td>
               <td>${book.author}</td>
               <td>${book.genre}</td>
               <td>${book.pages}</td>
-            </tr>
-          </table>`
-        );
-            list.innerHTML = html;           
-    };
-    listOfBook();
- 
-
-//we need an array to hold our state
-// const books = [];
-
-/*const handleSubmit = e => {
-    e.preventDefault();//prevent the page from reloading
-    const desOfBook = e.currentTarget.book.value;
-    if (!desOfBook) return;
-    console.log(desOfBook);
-    const book = {
-        title,
-        author,
-        page,
-    };
-    books.push(book);
-    console.log(`There are now ${books.length}`); 
-    e.target.reset();
+          </tr>`
+    ).join(' ');
+    list.innerHTML = html;
 };
 
-const displayList = () => {
+listedBook();
+
+const getListOfBook = () => {
     const html = books
         .map(
             book =>
-                `<li class="item">
-                <input
-                   value="${title.value}"/>
-                <input
-                   value="${author.value}"/>
-                <input
-                   value="${page.value}"/>
-                <span class="bookList">${book.desOfBook}</span>
-            </li>`
+                `<ul class="list-of-book">
+                    <input
+                       value="${title.value}"/>
+                    <input
+                       value="${author.value}"/>
+                    <input
+                       value="${pages.value}"/>
+                    <input
+                       value="${genre.value}"/>
+                    <span class="bookList">${book.listedBook}</span>
+                </ul>`
         )
         .join('');
     list.innerHTML = html;
 };
 
-descriptionForm.addEventListener('submit', handleSubmit);
-*/
+const handleSubmit = e => {
+    e.preventDefault;
+    const form = e.currentTarget;
+    const { title, author, pages, genre } = form;
+    // if (!form) return;
+
+    const newBook = {
+        title,
+        author,
+        pages,
+        genre,
+    }
+    allBooks.push(newBook);
+};
+
+addBooksForm.addEventListener('submit', handleSubmit);
