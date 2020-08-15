c3fcf6a2165523f0590b2a6af6405219a436e988
kati <fanomezantsoakatiuscia048@gmail.com>
2020-08-15 12:01:22 +0300

    accessed the value of the object array

--- script.js
+++ script.js
@@ -1,10 +1,54 @@
+const books = [
+    {
+        title: 'Harry Potter',
+        author: 'Jk',
+        genre: 'fantasy',
+        pages: 373,
+        read: true,
+    },
+    {
+        title: 'Clean code',
+        author: 'Robert',
+        genre: 'It',
+        pages: 435,
+        read: true,
+    },
+    {
+        title: 'Educated',
+        author: 'Tara Westover',
+        genre: 'memoir',
+        pages: 370,
+        read: false,
+    },
+];
+
+
 const descriptionForm = document.querySelector('.book-description');
 const list = document.querySelector('.list');
 
-//we need an array to hold our state
-const books = [];
 
-const handleSubmit = e => {
+//Generate list for all foods
+const listOfBook = (e) => {
+    //add html
+        const html = books.map (book => `
+          <table class= ".list">
+            <tr>
+              <td>${book.title}</td>
+              <td>${book.author}</td>
+              <td>${book.genre}</td>
+              <td>${book.pages}</td>
+            </tr>
+          </table>`
+        );
+            list.innerHTML = html;           
+    };
+    listOfBook();
+ 
+
+//we need an array to hold our state
+// const books = [];
+
+/*const handleSubmit = e => {
     e.preventDefault();//prevent the page from reloading
     const desOfBook = e.currentTarget.book.value;
     if (!desOfBook) return;
@@ -38,3 +82,4 @@ const displayList = () => {
 };
 
 descriptionForm.addEventListener('submit', handleSubmit);
+*/
